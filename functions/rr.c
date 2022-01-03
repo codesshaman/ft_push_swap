b4aef055df8f06f169e32be436e3c65f5a79ec30
Bick <90702473+codesshaman@users.noreply.github.com>
2022-01-03 16:44:39 +0300

    first push

--- functions/rr.c
+++ functions/rr.c
@@ -14,22 +14,6 @@
 
 void	rr(t_stack *stack)
 {
-	t_node	*nexta;
-	t_node	*nextb;
-	int		*buffa;
-	int		*buffb;
-
-	buffa = (int *) malloc(sizeof(int));
-	buffb = (int *) malloc(sizeof(int));
-	*buffa = *((int *) stack->a->first->content);
-	*buffb = *((int *) stack->b->first->content);
-	nexta = stack->a->first->next;
-	nextb = stack->b->first->next;
-	list_add_back(stack->a, &buffa);
-	list_add_back(stack->b, &buffb);
-	delete_list(stack->a, stack->a->first, free);
-	delete_list(stack->b, stack->b->first, free);
-	stack->a->first = nexta;
-	stack->b->first = nextb;
-	ft_putstr_fd("rr\n", 1);
+	ra(stack);
+	rb(stack);
 }
